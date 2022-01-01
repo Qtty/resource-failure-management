720b061537d47e17fcfe253e69591472d204533f
MOULOUDI Mohamed Lyes <ly3s@nc-ass-vip.sdv.fr>
2022-01-01 21:33:36 +0100

    Fixed a user creation issue

--- backend/init.py
+++ backend/init.py
@@ -14,16 +14,22 @@ def insert_users(col, users: list):
         col.insert_one(i)
 
 
-host = 'db'
+while True:
+    try:
+        host = 'db'
 
-with open("config.json", "r") as f:
-    config = loads(f.read())
+        with open("config.json", "r") as f:
+            config = loads(f.read())
 
-SALT = config["SALT"]
-client = pymongo.MongoClient(f"mongodb://{config['db']['username']}:{config['db']['password']}@{host}:27017/")
-db = client['resource-management']
-users_col = db['users']
+        SALT = config["SALT"]
+        client = pymongo.MongoClient(f"mongodb://{config['db']['username']}:{config['db']['password']}@{host}:27017/")
+        db = client['resource-management']
+        users_col = db['users']
 
-config['admin']['password'] = hash_pwd(SALT, config['admin']['password'])
-config['admin']['date'] = datetime.utcnow()
-insert_users(users_col, [config['admin']])
+        config['admin']['password'] = hash_pwd(SALT, config['admin']['password'])
+        config['admin']['date'] = datetime.utcnow()
+        insert_users(users_col, [config['admin']])
+
+        break
+    except Exception:
+        pass
